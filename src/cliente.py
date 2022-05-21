8d4d50876ec2e41b382eac0a4f9fcd71bdbbe806
luciaPardo <variosdemas@gmail.com>
2022-05-21 19:37:22 -0300

    Comienzo parseo header y checksum a checkear

--- src/cliente.py
+++ src/cliente.py
@@ -1,11 +1,21 @@
+from operator import add
 from socket import *
 
 serverName = '127.0.0.1'
 serverPort = 12000
 
+def calculateCheckSum(originPort, destPort, length):
+	return ~(originPort + destPort + length)
+
+def addHeader(message,clientSocket):
+    originPort = getnameinfo(clientSocket.getsockname(),NI_DGRAM)[1]
+    return str(serverPort) + str(originPort) + str(len(message)) + str(calculateCheckSum(int(originPort), serverPort, len(message))) + message
+
 clientSocket = socket(AF_INET, SOCK_DGRAM)
+clientSocket.bind(("localhost" , 0))
 message = input("Escriba una palabra: ")
-clientSocket.sendto(message.encode(), (serverName, serverPort))
+msg_complete = addHeader(message,clientSocket)
+clientSocket.sendto(msg_complete.encode(), (serverName, serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 print(modifiedMessage.decode())
 clientSocket.close()
--- src/servidor.py
+++ src/servidor.py
@@ -1,13 +1,44 @@
+from curses import resize_term
 from itertools import count
 from socket import *
+
+from rospy import Message
 
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
 
+
+class Message:
+    def __init__(self, message):
+        self.message = message
+        self.originPort = message[:2]
+        self.destPort  = message[2:4] 
+        self.length    = message[4:6]
+        self.checksum  = message[6:8] 
+        
+
+    def checkSumOk(self):
+        return self.checksum == self.calculateCheckSum()
+
+    def calculateCheckSum(self):
+        print(self.message)
+        for i in range(2): 
+            res =  self.originPort[i] + self.destPort[i] + self.length[i] + self.checksum[i]
+        print(res)
+        
+    
+        
+
+
 while True:
     message, clientAddress = serverSocket.recvfrom(2048)
+    mess = Message(message)
+    if  not mess.checkSumOk():
+        exit()
     message = len(message.decode())
     modifiedMessage = "La cantidad de letras que tiene la palabra es " + str(message)
     print(clientAddress)
     serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+
+
