cf409aa689dea4a07d7f176fe795e7fff9f7f18b
gmovia27uba <gmovia@fi.uba.ar>
2022-05-22 07:06:03 +0200

    Refactorizacion de cliente y servidor

--- src/cliente.py
+++ src/cliente.py
@@ -4,20 +4,50 @@ from socket import *
 serverName = '127.0.0.1'
 serverPort = 12000
 
+# originPort | serverPort | length | checkSum | message
+
 def calculateCheckSum(originPort, destPort, length):
 	return ~(originPort + destPort + length)
 
-def addHeader(message,clientSocket):
-    originPort = getnameinfo(clientSocket.getsockname(),NI_DGRAM)[1]
-    return str(serverPort) + str(originPort) + str(len(message)) + str(calculateCheckSum(int(originPort), serverPort, len(message))) + message
 
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-clientSocket.bind(("localhost" , 0))
-message = input("Escriba una palabra: ")
-msg_complete = addHeader(message,clientSocket)
-clientSocket.sendto(msg_complete.encode(), (serverName, serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print(modifiedMessage.decode())
-clientSocket.close()
+def getOriginPort(clientSocket):
+    return int(getnameinfo(clientSocket.getsockname(), NI_DGRAM)[1])
+
+
+def getLength(message):
+    return len(message) + 8
+
+
+def getStructureUDP(message, clientSocket):
+    originPort = getOriginPort(clientSocket)
+    length = getLength(message)
+    checkSum = calculateCheckSum(originPort, serverPort, length)
+    return [originPort, serverPort, length, checkSum, message]
+
+
+def addHeader(message, clientSocket):
+    structureUDP = getStructureUDP(message, clientSocket)
+
+    segment = ""
+    for i in range(len(structureUDP)):
+        segment = str(segment) + str(structureUDP[i])
+    return segment
+
+
+def mainClient():
+    clientSocket = socket(AF_INET, SOCK_DGRAM)
+    clientSocket.bind(("localhost" , 0))
+    message = input("Escriba una palabra: ")
+   
+    segment = addHeader(message, clientSocket)
+    print(segment)
+    
+    clientSocket.sendto(segment.encode(), (serverName, serverPort))
+    newMessage, serverAddress = clientSocket.recvfrom(2048)
+    
+    print(newMessage.decode())
+    clientSocket.close()
+
+mainClient()
 
 
--- src/servidor.py
+++ src/servidor.py
@@ -1,15 +1,9 @@
 from curses import resize_term
 from itertools import count
 from socket import *
+#from rospy import Message
 
-from rospy import Message
-
-serverPort = 12000
-serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(('', serverPort))
-
-
-class Message:
+class Segment:
     def __init__(self, message):
         self.message = message
         self.originPort = message[:2]
@@ -18,27 +12,31 @@ class Message:
         self.checksum  = message[6:8] 
         
 
-    def checkSumOk(self):
-        return self.checksum == self.calculateCheckSum()
+    def checkSumOk(self): # falla el checkout, y no se xq
+        return hex(int(self.calculateCheckSum(), 16)) == 0xffff 
+
 
     def calculateCheckSum(self):
-        print(self.message)
-        for i in range(2): 
-            res =  self.originPort[i] + self.destPort[i] + self.length[i] + self.checksum[i]
-        print(res)
+        print(self.message) # podemos comprobar que recibimos bien el mensaje !
+        return self.originPort.decode() + self.destPort.decode() + self.length.decode() + self.checksum.decode()
         
     
-        
+def mainServer():
+    serverPort = 12000
+    serverSocket = socket(AF_INET, SOCK_DGRAM)
+    serverSocket.bind(('', serverPort))
 
+    while True:
+        segment, clientAddress = serverSocket.recvfrom(2048)
+        segment = Segment(segment)
 
-while True:
-    message, clientAddress = serverSocket.recvfrom(2048)
-    mess = Message(message)
-    if  not mess.checkSumOk():
-        exit()
-    message = len(message.decode())
-    modifiedMessage = "La cantidad de letras que tiene la palabra es " + str(message)
-    print(clientAddress)
-    serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+        if not segment.checkSumOk():
+            print("Error checkout")
+            exit()
 
+        message = len(segment.message.decode())
+        modifiedMessage = "La cantidad de letras que tiene la palabra es " + str(message)
+        serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+
+mainServer()
 
