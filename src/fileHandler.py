6ac59b316bef95e57991daae5e246a31b2cde7a5
Cbravor1991 <cbravor@fi.uba.ar>
2022-05-30 17:20:16 -0300

    cambios filehandler

--- src/fileHandler.py
+++ src/fileHandler.py
@@ -5,18 +5,34 @@ class FileHandler:
 
     def getSizeFile (self, path):
         return os.path.getsize(path)
-        
+    
+
+    
 
     def readFileBytes(self, filepath, bytesToRead):
         file = open(filepath, 'rb')
-        contents = file.read(bytesToRead)
+        filePosition = file.tell()
+        if (filePosition < self.getSizeFile(self, filepath)):
+            rest= filePosition- bytesToRead
+            print (rest)
+            if (rest< bytesToRead):
+                contents = file.read(1)
+            else:
+                contents = file.read(bytesToRead)
+
         return contents
 
 
+def main(self):
+
+    file_name = 'name'
+    path = './texto.txt'
+    file_size = self.getSizeFile(self, path)  
+    contenido = self.readFileBytes(self, path, 5)
 
 
+    print(file_size)
+    print (contenido)
 
-
-    
-    
+main(FileHandler)
     
