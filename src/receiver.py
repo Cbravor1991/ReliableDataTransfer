906fb169c059c5f14ec9ecddfdef733dc1ae7991
RVLareu <rlareu@fi.uba.ar>
2022-06-02 17:22:33 -0300

    El que recibe escribe el archivo, falta que reciba este primer comando

--- src/receiver.py
+++ src/receiver.py
@@ -1,20 +1,42 @@
 from lib.protocol import Protocol
 from lib.socketUDP import SocketUDP
-
+import math
 
 class Receiver:
     def __init__(self):
         self.window_size = 200
         self.window_start = 0
-        self.messagesBuffer = [False for i in range(50000)]
+        self.messagesBuffer = []
         self.socket = SocketUDP()
         self.protocol = Protocol()
 
     def bindSocket(self, host, port):
         self.socket.bindSocket(host, port)
 
+    def getTopOfWindow(self, file_size):
+        window_top = self.window_start + self.window_size
+        file_top = math.ceil(file_size/200)
+        if (window_top > file_top):
+            return file_top
+        else:
+            return window_top
+
+
     def receive(self):
-        while True:
+
+        
+        #segment, clientAddr = self.protocol.receive(self.socket)
+        #fileSize, fileName = self.protocol.processUploadSegment(segment)
+        #ACKMessage = self.protocol.createACKMessage(0)
+        #self.protocol.sendMessage(self.socket, clientAddr, ACKMessage)
+        fileName = "Elarhivo0.pdf"
+        fileSize = 458533
+
+        file = open(fileName, "wb")
+        counter = 0
+        self.messagesBuffer = [False for i in range(math.ceil(fileSize/200))]
+        while counter < math.ceil(fileSize/200):
+
             segment, clientAddr = self.protocol.receive(self.socket)
 
             seqNum, data = self.protocol.processRecPackageSegment(segment)
@@ -28,11 +50,15 @@ class Receiver:
                       .format(seqNum, data))
                 self.messagesBuffer[seqNum] = data
                 if (seqNum == self.window_start):
+                    counter += 1
+                    file.write(self.messagesBuffer[seqNum])
                     self.window_start += 1
                     i = self.window_start
-                    while i < self.window_start + self.window_size:
+                    while i < self.getTopOfWindow(fileSize):
                         if (self.messagesBuffer[i] is not False):
                             self.window_start += 1
+                            counter += 1
+                            file.write(self.messagesBuffer[i])
                         else:
                             break
                         i += 1
@@ -41,4 +67,5 @@ class Receiver:
                 self.protocol.sendMessage(self.socket, clientAddr, ACKMessage)
             else:
                 pass
-            
+            print(counter)
+        file.close()
