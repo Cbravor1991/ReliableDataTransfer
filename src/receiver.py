05f4c1b495026a84579a1cc822a3bc23f71b5ef5
RVLareu <rlareu@fi.uba.ar>
2022-06-02 12:15:48 -0300

    receiver encapsulado

--- src/receiver.py
+++ src/receiver.py
@@ -1,50 +1,44 @@
 from lib.protocol import Protocol
 from lib.socketUDP import SocketUDP
 
+
 class Receiver:
-    def __init__(self) -> None:
+    def __init__(self):
         self.window_size = 5
         self.window_start = 0
         self.messagesBuffer = [False for i in range(1000)]
+        self.socket = SocketUDP()
+        self.protocol = Protocol()
 
+    def bindSocket(self, host, port):
+        self.socket.bindSocket("localhost", 12000)
 
+    def receive(self):
+        while True:
 
-def main():
-    
-    serverSocket = SocketUDP()
-    serverSocket.bindSocket("localhost", 12000)
-    protocol = Protocol()
-    receiver = Receiver()
+            segment, clientAddr = self.protocol.receive(self.socket)
 
+            seqNum, cs, data = self.protocol.processRecPackageSegment(segment)
 
-    while True: # hasta terminar el upload o que haya algun error
-        
-        segment, clientAddr = protocol.receive(serverSocket)
-        
-        sequenceNumber, checkSum, data = protocol.processRecPackageSegment(segment)
-         
+            if (seqNum >= self.window_start and
+               seqNum < self.window_size + self.window_start):
 
-        if (sequenceNumber >= receiver.window_start and sequenceNumber < receiver.window_size + receiver.window_start):
-            ACKMessage = protocol.createACKMessage(sequenceNumber)
-            protocol.sendMessage(serverSocket, clientAddr, ACKMessage)
-            print('Sequence number {}, data: {}'.format(sequenceNumber,data))
-            receiver.messagesBuffer[sequenceNumber] = data
-            if (sequenceNumber == receiver.window_start):
-                receiver.window_start += 1
-                i = receiver.window_start 
-                while i < receiver.window_start + receiver.window_size:
-                    if (receiver.messagesBuffer[i] != False):
-                        receiver.window_start += 1
-                    else:
-                        break
-                    i += 1
-        elif (sequenceNumber < receiver.window_start):
-            ACKMessage = protocol.createACKMessage(sequenceNumber)
-            protocol.sendMessage(serverSocket, clientAddr, ACKMessage)
-        else:
-            pass
-
-       
-
-
-main()
+                ACKMessage = self.protocol.createACKMessage(seqNum)
+                self.protocol.sendMessage(self.socket, clientAddr, ACKMessage)
+                print('Sequence number {}, data: {}'
+                      .format(seqNum, data))
+                self.messagesBuffer[seqNum] = data
+                if (seqNum == self.window_start):
+                    self.window_start += 1
+                    i = self.window_start
+                    while i < self.window_start + self.window_size:
+                        if (self.messagesBuffer[i] is not False):
+                            self.window_start += 1
+                        else:
+                            break
+                        i += 1
+            elif (seqNum < self.window_start):
+                ACKMessage = self.protocol.createACKMessage(seqNum)
+                self.protocol.sendMessage(self.socket, clientAddr, ACKMessage)
+            else:
+                pass
