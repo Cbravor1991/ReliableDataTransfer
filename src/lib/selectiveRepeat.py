70193cf7e78c30c1e3ae6c04a11b2fa6f5a2cc2a
RVLareu <rlareu@fi.uba.ar>
2022-06-03 12:31:53 -0300

    server upload en SR

--- src/lib/selectiveRepeat.py
+++ src/lib/selectiveRepeat.py
@@ -6,7 +6,7 @@ from lib.protocol import Protocol
 from receiver import Receiver
 from sender import Sender
 from lib.fileHandler import FileHandler
-
+import math
 class SelectiveRepeat:
 
 
@@ -14,39 +14,90 @@ class SelectiveRepeat:
         self.protocol = Protocol()
     
 
-    def upload(self, clientSocket, filename, file, fileSize, serverAddr):
+    def getTopOfWindow(self, segmentsToReceive, window_start, window_size):
+        window_top = window_start + window_size
+        if (window_top > segmentsToReceive):
+            return segmentsToReceive
+        else:
+            return window_top
+
+    def isInsideWindow(self, window_start, window_size, value):
+        return (value >= window_start) and (value < window_size + window_start)
+
+    def isBelowWindow(self, seqNum, window_start):
+        return seqNum < window_start
+
+
+    def clientUpload(self, clientSocket, filename, file, fileSize, serverAddr):
         sender = Sender(file, serverAddr[1], fileSize)
         sender.startClient(clientSocket, filename, file, fileSize, serverAddr)
 
-    def download(self, clientSocket, fileName, path, serverAddr):
+    def clientDownload(self, clientSocket, fileName, path, serverAddr):
         receiver = Receiver()
-        receiver.receiveFileFromServer( clientSocket, fileName, path, serverAddr)
+        receiver.receiveFileFromServer(clientSocket, fileName, path, serverAddr)
 
-    def sendFileToClient(self, segment, serverSocket, clientAddr):
+    def serverDownload(self, recvQueue, sendQueue, clientAddr, dstPath):
+
+        segment = recvQueue.get()
         fileName = self.protocol.processDownloadSegment(segment)
-        
 
         print('command {} fileName {}'.format(segment[0], fileName))
-        path = './' + fileName
-        print(path)
+        path = dstPath + fileName
+
         try:
             file = FileHandler.openFile(path)
+            fileSize = FileHandler.getFileSize(path)
         except:
             print('File not found')
             return
-        
-        fileSize = FileHandler.getFileSize(path)
+
         fileSizeSegment = Encoder.createFileSize(fileSize)
-        self.protocol.sendMessage(serverSocket, clientAddr, fileSizeSegment)
-
-        print("FUILE SIZE: {}".format(fileSize))
-        sender = Sender(file, 5, fileSize )
-        sender.startServer(segment, serverSocket, clientAddr)
+        sendQueue.put((fileSizeSegment, clientAddr))
+        
+        #sender = Sender(file, 5, fileSize )
+        #sender.startServer(segment, serverSocket, clientAddr)
     
-    def receiveFileFromClient(self, segment, clientAddr, serverSocket):
-        receiver = Receiver()
-        receiver.receive(segment, clientAddr, serverSocket)
 
+
+    def serverUpload(self,recvQueue, sendQueue, clientAddr, dstPath):
+        window_size = 3
+        window_start = 0
+        MSS = 5
+        
+        segment = recvQueue.get()
+        fileSize, fileName = self.protocol.processUploadSegment(segment)
+        segmentsToReceive = math.ceil(fileSize/MSS)
+        messagesBuffer = [False for i in range(segmentsToReceive)]
+
+
+        ACKMessage = self.protocol.createACKMessage(0)
+        sendQueue.put((ACKMessage, clientAddr))
+        file = FileHandler.newFile(dstPath, fileName)
+        
+        while window_start < segmentsToReceive:
+            segment = recvQueue.get()
+            if (Decoder.isRecPackage(segment)):
+                seqNum, data = self.protocol.processRecPackageSegment(segment)
+                if (self.isInsideWindow(window_start, window_size, seqNum)):
+                    ACKMessage = self.protocol.createACKMessage(seqNum)
+                    sendQueue.put((ACKMessage, clientAddr))
+                    messagesBuffer[seqNum] = data
+                    # verifico buffereados
+                    if (seqNum == window_start):
+                        file.write(messagesBuffer[seqNum])
+                        window_start += 1
+                        while window_start < self.getTopOfWindow(fileSize):
+                                if (messagesBuffer[window_start] is not False):
+                                    file.write(messagesBuffer[window_start])
+                                    window_start += 1
+                                else:
+                                    break
+                elif (self.isBelowWindow(self, seqNum, window_start)):
+                    ACKMessage = self.protocol.createACKMessage(seqNum)
+                    sendQueue.put((ACKMessage, clientAddr))
+            elif (Decoder.isUpload(segment)):
+                sendQueue.put((ACKMessage, clientAddr))
+        file.close()
 
     
     
--- src/senderMain.py
+++
@@ -1,9 +0,0 @@
-from sender import Sender
-
-
-def main():
-    sender = Sender('./tp_file_transfer_udp_selective_repeat.pdf', 12000)
-    sender.start()
-
-
-main()
